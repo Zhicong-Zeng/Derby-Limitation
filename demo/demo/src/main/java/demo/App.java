8196eb2e77172b96f6b1a3624579eebe29a87e24
Zhicong Zeng <zzeng1@seattleu.edu>
2022-05-26 21:59:19 -0700

    Trying to add mysql

--- demo/demo/src/main/java/demo/App.java
+++ demo/demo/src/main/java/demo/App.java
@@ -12,9 +12,7 @@ import java.sql.Statement;
  */
 public class App {
     Connection connDerby;
-    Connection connMysql;
-    String connectionUrl = "jdbc:mysql://127.0.0.1:3306/test";
-
+    
     public static void main(String[] args) throws SQLException {
         App appDerby = new App();
         appDerby.connectionDerby();
@@ -22,15 +20,14 @@ public class App {
         //appDerby.testColumns();
         //appDerby.testConstraintName();
 
-        App appMysql = new App();
         // appMysql.connectionMysql();
-        appMysql.mysqltestRows();
+        mysqltestRows();
     }
 
     public void connectionDerby() throws SQLException {
         // URL format is
         // jdbc:derby:<local directory to save data>
-        String dbUrl = "jdbc:derby:Data;create=true";
+        String dbUrl = "jdbc:derby:Derby;create=true";
         connDerby = DriverManager.getConnection(dbUrl, "root", "root");
     }
 
@@ -127,86 +124,30 @@ public class App {
     }
 
     //MaxRows is unlimitation accoring to disk
-    public void mysqltestRows() throws SQLException {
-        String sql = "CREATE TABLE REGISTRATION " +
+    public static void mysqltestRows() throws SQLException {
+        String createsql = "CREATE TABLE REGISTRATION " +
         "(id INTEGER not NULL, " +
         " first VARCHAR(255), " + 
         " last VARCHAR(255), " + 
         " age INTEGER, " + 
         " PRIMARY KEY ( id ))"; 
 
-        
+        String insertsql = " insert into REGISTRATION (id , first, last, age)"
+                         + " values (1, 1, 1, 1)";
 
-        try(Connection conn = DriverManager.getConnection(connectionUrl, "root", "password"); 
-            Statement stmt = conn.createStatement();) 
-        {		      
-            stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");   	  
+        String selectsql = "SELECT * FROM REGISTRATION";
+
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+            Statement stmt = conn.createStatement())
+        {		     
+            stmt.executeQuery(createsql);
+            stmt.executeQuery(insertsql);
+            ResultSet rs = stmt.executeQuery(selectsql);
+            while (rs.next()) {
+                System.out.printf("%d\t%s\n", rs.getInt("id"), rs.getString("column_1"), rs.getString("column_2"), rs.getInt("age"));
+            }	  
         } catch (SQLException e) {
             e.printStackTrace();
         } 
-
-
-
-        
-    //     try{
-    //         Connection conn = DriverManager.getConnection(connectionUrl, "root", "password"); 
-    //         PreparedStatement preparedStatement = conn.prepareStatement(sql);
-    //         ResultSet resultSet = preparedStatement.executeQuery();
-    //     } catch (SQLException e) 
-    //     {
-
-    //     }
-        
-    //     try{
-    //         // create a mysql database connection
-    //         String myDriver = "org.gjt.mm.mysql.Driver";
-    //         String myUrl = "jdbc:mysql://localhost/test";
-    //         Class.forName(myDriver);
-    //         Connection conn = DriverManager.getConnection(myUrl, "root", "");
-
-    //         // the mysql insert statement
-    //         String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)"
-    //                     + " values (?, ?, ?, ?, ?)";
-
-    //         // create the mysql insert preparedstatement
-    //         PreparedStatement preparedStmt = conn.prepareStatement(query);
-    //         preparedStmt.setString (1, "Barney");
-    //         preparedStmt.setString (2, "Rubble");
-    //         preparedStmt.setDate   (3, startDate);
-    //         preparedStmt.setBoolean(4, false);
-    //         preparedStmt.setInt    (5, 5000);
-
-    //         // execute the preparedstatement
-    //         preparedStmt.execute();
-      
-    //         conn.close();
-            
-    //         while (resultSet.next()) {
-    //             System.out.println(resultSet.getInt(1)+"  "+resultSet.getString(2)+"  "+resultSet.getString(3));  
-    //         }
-    //     }catch (Exception e){
-    //         System.err.println("Got an exception!");
-    //         System.err.println(e.getMessage());
-    //     }
-    // }
-        
-
-    //     stmt.executeUpdate(sql);
-
-    //     sql = "INSERT INTO REGISTRATION VALUES (100, 'Zara', 'Ali', 18)";
-    //     stmt.executeUpdate(sql);
-        
-    //     sql = "SELECT id, first, last, age FROM Registration";
-    //     ResultSet rs = stmt.executeQuery(sql);
-
-    //     // print out query result
-    //     while(rs.next()){
-    //         //Display values
-    //         System.out.print("ID: " + rs.getInt("id"));
-    //         System.out.print(", Age: " + rs.getInt("age"));
-    //         System.out.print(", First: " + rs.getString("first"));
-    //         System.out.println(", Last: " + rs.getString("last"));
-    //     }
     }
 }
